c88f8f4fe15feb6ff54a13188143c120d84041ed
ron71 <maxphinix@gmail.com>
2020-08-14 23:00:23 +0530

    Version1

--- rating-data-service/src/main/java/com/cozinfinitybehind/ratingdataservice/resources/RatingsDataResource.java
+++ rating-data-service/src/main/java/com/cozinfinitybehind/ratingdataservice/resources/RatingsDataResource.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
-@RequestMapping("/ratingdata")
+@RequestMapping("/ratingsdata")
 public class RatingsDataResource {
 
     @RequestMapping("/{movieId}")
